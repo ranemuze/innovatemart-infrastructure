ab8a7096703e0735ed182f6d6e533bdefad25001
Rane Muze <muzerane@gmail.com>
2025-09-25 23:17:17 +0100

    Add CI/CD infrastructure

--- backend.tf
+++ backend.tf
@@ -1,16 +1,9 @@
-# Terraform Backend Configuration
 terraform {
   backend "s3" {
-    # These values will be provided via backend-config during terraform init
-    # bucket = "innovatemart-terraform-state"
-    # key    = "innovatemart/terraform.tfstate"
-    # region = "eu-west-1"
-    
-    # Enable state locking and consistency checking
+    bucket         = "innovatemart-terraform-state-12f7148b"
+    key            = "innovatemart/terraform.tfstate"
+    region         = "eu-west-1"
     dynamodb_table = "innovatemart-terraform-locks"
     encrypt        = true
-    
-    # Workspace configuration
-    workspace_key_prefix = "env"
   }
 }
